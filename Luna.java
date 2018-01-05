3fdfcce8aec71cf717d22044f01ce00901669c6c
ZTRD <35132922+ZTRD@users.noreply.github.com>
2018-01-05 02:19:54 -0800

    Created Luna.java
    The beginning of Luna.

---
+++ Luna.java
@@ -0,0 +1,30 @@
+package Main;
+import java.time.*;
+//import another library for user input
+
+public class Luna {
+  
+	public Luna(){
+  }
+  
+  public String getTime(LocalTime obj){
+  	return "The time is currently "+obj.getHour()+":"+obj.getMinute()+":"+obj.getSecond();
+  }
+  //(obj.getHour()<10) ? "0"+obj.getHour() : obj.getHour() <- Ternary Operator
+  
+  /* public String LessThan(int time)
+  	 {
+  	   return (time<10) ? "0"+time : time;
+  	 }
+	*/
+  // return "The time is currently "+LessThan(obj.getHour())+":"+LessThan(obj.getMinute())+":"+LessThan(obj.getSecond());
+  
+	public static void main (String[] args){
+		Luna Unus = new Luna();
+    LocalTime localTime = LocalTime.now();
+    // User Input
+    // if USER INPUT = I want time; Then Display + witty response
+		System.out.println(Unus.getTime(localTime));
+	}
+
+}
